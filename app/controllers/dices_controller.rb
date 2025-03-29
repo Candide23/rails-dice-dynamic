e59983bb5bb04dc3e63167b0222b4aa439f4c7b7
Candide Mboungou-Kimpolo <89795037+Candide23@users.noreply.github.com>
2025-03-29 15:55:53 +0000

    added two_six business logic

--- app/controllers/dices_controller.rb
+++ app/controllers/dices_controller.rb
@@ -3,4 +3,16 @@ class DicesController < ApplicationController
   def home
     render(template: "templates/home")
   end
+
+  def two_six
+    @rolls = []
+
+    2.times do
+      die = rand(2..6)
+  
+      @rolls.push(die)
+    end
+    render(template: "templates/two_six")
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
 get "/", {controller: "dices", action: "home"}
+get "/dice/2/6", {controller: "dices", action: "two_six"}
 
 end
